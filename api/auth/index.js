29d3f4cc37ac45dd0a5dffec6af42d233a12e33a
Alexandre Lang <alexandre.lang@bearingpoint.com>
2025-06-23 02:17:08 +0200

    Fix proxies to use CommonJS and native fetch

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,20 +1,25 @@
-﻿import fetch from 'node-fetch';
+﻿module.exports = async function (context, req) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-export default async function(context, req) {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-  const path = context.bindingData.path || '';
-  const url = `https://4.231.232.226:8443/auth/${path}`;
+  const path = context.bindingData.path || ''
+  const url = `https://4.231.232.226:8443/auth/${path}` +
+    (req.originalUrl.includes('?')
+      ? '?' + req.originalUrl.split('?')[1]
+      : '')
 
   const resp = await fetch(url, {
     method: req.method,
     headers: req.headers,
-    body: ['GET','HEAD'].includes(req.method) ? undefined : req.rawBody
-  });
+    body:
+      ['GET', 'HEAD', 'OPTIONS'].includes(req.method) || !req.rawBody
+        ? undefined
+        : req.rawBody
+  })
 
+  const buffer = await resp.arrayBuffer()
   context.res = {
     status: resp.status,
     headers: Object.fromEntries(resp.headers.entries()),
-    body: await resp.buffer()
-  };
+    body: Buffer.from(buffer)
+  }
 }
--- api/rest/index.js
+++ api/rest/index.js
@@ -1,22 +1,27 @@
-﻿import fetch from 'node-fetch';
-
-export default async function(context, req) {
+﻿module.exports = async function (context, req) {
   // ignorer le TLS auto-signé
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-  const path = context.bindingData.path || '';
-  const query = req.originalUrl.split('?')[1] || '';
-  const url = `https://4.231.232.226:8443/rest/${path}${query ? '?' + query : ''}`;
+  const path = context.bindingData.path || ''
+  const query = req.originalUrl.includes('?')
+    ? '?' + req.originalUrl.split('?')[1]
+    : ''
+  const url = `https://4.231.232.226:8443/rest/${path}${query}`
 
+  // utilise fetch global de Node 18+
   const resp = await fetch(url, {
     method: req.method,
     headers: req.headers,
-    body: ['GET','HEAD'].includes(req.method) ? undefined : req.rawBody
-  });
+    body:
+      ['GET', 'HEAD', 'OPTIONS'].includes(req.method) || !req.rawBody
+        ? undefined
+        : req.rawBody
+  })
 
+  const buffer = await resp.arrayBuffer()
   context.res = {
     status: resp.status,
     headers: Object.fromEntries(resp.headers.entries()),
-    body: await resp.buffer()
-  };
+    body: Buffer.from(buffer)
+  }
 }
