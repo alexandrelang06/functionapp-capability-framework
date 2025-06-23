c2e88588597cd522fa9e7500c00e54b7d898e5a5
Alexandre Lang <alexandre.lang@bearingpoint.com>
2025-06-23 02:01:58 +0200

    Supprime staticwebapp.config.json et ajout des proxies Azure Functions

---
+++ api/auth/index.js
@@ -0,0 +1,20 @@
+﻿import fetch from 'node-fetch';
+
+export default async function(context, req) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  const path = context.bindingData.path || '';
+  const url = `https://4.231.232.226:8443/auth/${path}`;
+
+  const resp = await fetch(url, {
+    method: req.method,
+    headers: req.headers,
+    body: ['GET','HEAD'].includes(req.method) ? undefined : req.rawBody
+  });
+
+  context.res = {
+    status: resp.status,
+    headers: Object.fromEntries(resp.headers.entries()),
+    body: await resp.buffer()
+  };
+}
---
+++ api/rest/index.js
@@ -0,0 +1,22 @@
+﻿import fetch from 'node-fetch';
+
+export default async function(context, req) {
+  // ignorer le TLS auto-signé
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  const path = context.bindingData.path || '';
+  const query = req.originalUrl.split('?')[1] || '';
+  const url = `https://4.231.232.226:8443/rest/${path}${query ? '?' + query : ''}`;
+
+  const resp = await fetch(url, {
+    method: req.method,
+    headers: req.headers,
+    body: ['GET','HEAD'].includes(req.method) ? undefined : req.rawBody
+  });
+
+  context.res = {
+    status: resp.status,
+    headers: Object.fromEntries(resp.headers.entries()),
+    body: await resp.buffer()
+  };
+}
